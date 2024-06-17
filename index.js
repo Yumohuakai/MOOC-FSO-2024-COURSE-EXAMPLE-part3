6802246be1a8c4ab1ff82a4b4af9e85e00bdcdd3
yumohuakai <yumohuakai@gmail.com>
2024-06-17 13:21:21 +0300

    FSO - part3.c.8. Moving db configuration to its own module

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
-
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://fullstack-linjing:${password}@cluster0.mngw1ve.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.set("strictQuery", false);
-
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Note = mongoose.model("Note", noteSchema);
 
 // let notes = [
 //   {
@@ -129,7 +102,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
